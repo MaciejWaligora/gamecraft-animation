9990f07fee61756e32cc54013ec0bcb49239135f
Maciej Waligóra <100129859+MaciejWaligora@users.noreply.github.com>
2024-05-25 09:18:23 +0200

    chore: version bump

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AnimationManager = void 0;
+exports.Animation = exports.AnimationManager = void 0;
 var AnimationManager_1 = require("./AnimationManager");
 Object.defineProperty(exports, "AnimationManager", { enumerable: true, get: function () { return AnimationManager_1.AnimationManager; } });
+var Animation_1 = require("./Animations/Animation");
+Object.defineProperty(exports, "Animation", { enumerable: true, get: function () { return Animation_1.Animation; } });
