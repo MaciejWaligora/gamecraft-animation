c46e00cece3067202e213fe5f86b04824fbe13ce
Maciej Waligóra <100129859+MaciejWaligora@users.noreply.github.com>
2024-05-25 10:18:27 +0200

    chore: added more exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,10 @@
 export { AnimationManager, AnimationManagerConfig } from "./AnimationManager";
-export { Animation, AnimationConfig } from "./Animations/Animation";
+export * from "./Animations/Animation";
+export * from "./Animations/ConstantMoveAnimation";
+export * from "./Animations/MoveAnimation";
+export * from "./Animations/PopAnimation";
+export * from "./Animations/SlideInAnimiation";
+export * from "./Animations/SlideInfromLeftAnimation";
+export * from "./Animations/SlideOutToRightAnimation";
+export * from "./Animations/SpinAnimation";
+export * from "./Animations/TiltAnimation";
